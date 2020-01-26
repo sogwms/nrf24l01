0652ef580cc26f2b3ea01753f92b1936731731ce
sogw <sogwyms@gmail.com>
2020-01-26 09:46:36 +0800

    style: standardize

--- src/nrf24l01.c
+++ src/nrf24l01.c
@@ -112,25 +112,23 @@ extern hal_nrf24l01_port_t hal_nrf24l01_port;
 
 /* Exported functions ----------------------------------------------------------------------*/  
 
-
-
-static uint8_t _read_reg(uint8_t reg)
+static uint8_t __read_reg(uint8_t reg)
 {
-    uint8_t temp, rtmp = 0;
+    uint8_t tmp, rtmp = 0;
 
-    temp = NRF24CMD_R_REG | reg;
-    hal_nrf24l01_port.send_then_recv(&temp, 1, &rtmp, 1);
+    tmp = NRF24CMD_R_REG | reg;
+    hal_nrf24l01_port.send_then_recv(&tmp, 1, &rtmp, 1);
 
     return rtmp;
 }
 
-static void _write_reg(uint8_t reg, uint8_t data)
+static void __write_reg(uint8_t reg, uint8_t data)
 {
-    uint8_t temp[2];
+    uint8_t tmp[2];
 
-    temp[0] = NRF24CMD_W_REG | reg;
-    temp[1] = data;
-    hal_nrf24l01_port.write(&temp[0], 2);
+    tmp[0] = NRF24CMD_W_REG | reg;
+    tmp[1] = data;
+    hal_nrf24l01_port.write(&tmp[0], 2);
 }
 
 /**
@@ -138,75 +136,75 @@ static void _write_reg(uint8_t reg, uint8_t data)
  * @param[in] reg: 寄存器地址
  * @param[in] mask: 位掩码. eg: 0x81 标识置位第七位和第零位
  */
-static void _set_reg_bits(uint8_t reg, uint8_t mask)
+static void __set_reg_bits(uint8_t reg, uint8_t mask)
 {
-    uint8_t temp;
+    uint8_t tmp;
 
-    temp = _read_reg(reg);
-    temp |= mask;
-    _write_reg(reg, temp);
+    tmp = __read_reg(reg);
+    tmp |= mask;
+    __write_reg(reg, tmp);
 }
 
-static void _reset_reg_bits(uint8_t reg, uint8_t mask)
+static void __reset_reg_bits(uint8_t reg, uint8_t mask)
 {
-    uint8_t temp;
+    uint8_t tmp;
 
-    temp = _read_reg(reg);
-    temp &= ~mask;
-    _write_reg(reg, temp);
+    tmp = __read_reg(reg);
+    tmp &= ~mask;
+    __write_reg(reg, tmp);
 }
 
-static void _write_reg_bits(uint8_t reg, uint8_t mask, uint8_t value)
+static void __write_reg_bits(uint8_t reg, uint8_t mask, uint8_t value)
 {
-    uint8_t temp, tidx;
+    uint8_t tmp, tidx;
 
     for (tidx = 0; tidx < 8; tidx++)
     {
         if (mask & (1 << tidx))
             break;
     }
-    temp = ~mask & _read_reg(reg);
-    temp |= mask & (value << tidx);
-    _write_reg(reg, temp);
+    tmp = ~mask & __read_reg(reg);
+    tmp |= mask & (value << tidx);
+    __write_reg(reg, tmp);
 }
 
 static void send_activate_command(void)
 {
-    uint8_t temp[2] = {
+    uint8_t tmp[2] = {
         NRF24CMD_ACTIVATE,
         0x73};
-    hal_nrf24l01_port.write(temp, 2);
+    hal_nrf24l01_port.write(tmp, 2);
 }
 
 static void set_address_width5(void)
 {
-    _write_reg(NRF24REG_SETUP_AW, 0x3);
+    __write_reg(NRF24REG_SETUP_AW, 0x3);
 }
 
 static void set_tx_rp0_address5(const uint8_t *pb)
 {
-    uint8_t temp;
+    uint8_t tmp;
 
-    temp = NRF24CMD_W_REG | NRF24REG_RX_ADDR_P0;
-    hal_nrf24l01_port.send_then_send(&temp, 1, pb, 5);
-    temp = NRF24CMD_W_REG | NRF24REG_TX_ADDR;
-    hal_nrf24l01_port.send_then_send(&temp, 1, pb, 5);
+    tmp = NRF24CMD_W_REG | NRF24REG_RX_ADDR_P0;
+    hal_nrf24l01_port.send_then_send(&tmp, 1, pb, 5);
+    tmp = NRF24CMD_W_REG | NRF24REG_TX_ADDR;
+    hal_nrf24l01_port.send_then_send(&tmp, 1, pb, 5);
 }
 
 static void set_rf_channel(uint8_t channel)
 {
-    _write_reg(NRF24REG_RF_CH, channel & 0x7F);
+    __write_reg(NRF24REG_RF_CH, channel & 0x7F);
 }
 
 static void set_air_data_rate(nrf24_adr_et adr)
 {
     if (adr == ADR_1Mbps)
     {
-        _reset_reg_bits(NRF24REG_RF_SETUP, NRF24BITMASK_RF_DR);
+        __reset_reg_bits(NRF24REG_RF_SETUP, NRF24BITMASK_RF_DR);
     }
     else if (adr == ADR_2Mbps)
     {
-        _set_reg_bits(NRF24REG_RF_SETUP, NRF24BITMASK_RF_DR);
+        __set_reg_bits(NRF24REG_RF_SETUP, NRF24BITMASK_RF_DR);
     }
 }
 
@@ -217,37 +215,37 @@ static void set_rf_power(nrf24_power_et pa)
         (pa == RF_POWER_N12dBm) ||
         (pa == RF_POWER_N18dBm))
     {
-        _write_reg_bits(NRF24REG_RF_SETUP, NRF24BITMASK_RF_PWR, pa);
+        __write_reg_bits(NRF24REG_RF_SETUP, NRF24BITMASK_RF_PWR, pa);
     }
 }
 
 static void _set_auto_retransmit_delay(uint8_t ard)
 {
-    _write_reg_bits(NRF24REG_SETUP_RETR, NRF24BITMASK_ARD, ard);
+    __write_reg_bits(NRF24REG_SETUP_RETR, NRF24BITMASK_ARD, ard);
 }
 
 static void _set_auto_retransmit_count(uint8_t arc)
 {
-    _write_reg_bits(NRF24REG_SETUP_RETR, NRF24BITMASK_ARC, arc);
+    __write_reg_bits(NRF24REG_SETUP_RETR, NRF24BITMASK_ARC, arc);
 }
 
 static void set_crc(nrf24_crc_et crc)
 {
     if (crc == CRC_0_BYTE)
     {
-        _reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_EN_CRC);
+        __reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_EN_CRC);
     }
     else
     {
         if (crc == CRC_1_BYTE)
         {
-            _set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_EN_CRC);
-            _reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_CRCO);
+            __set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_EN_CRC);
+            __reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_CRCO);
         }
         else if (crc == CRC_2_BYTE)
         {
-            _set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_EN_CRC);
-            _set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_CRCO);
+            __set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_EN_CRC);
+            __set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_CRCO);
         }
     }
 }
@@ -264,22 +262,22 @@ static void set_esb_param(nrf24_esb_t *pt)
 // bit: RX_DR, TX_DS, MAX_RT
 static void reset_status(uint8_t bitmask)
 {
-    bitmask |= _read_reg(NRF24REG_STATUS);
-    _write_reg(NRF24REG_STATUS, bitmask);
+    bitmask |= __read_reg(NRF24REG_STATUS);
+    __write_reg(NRF24REG_STATUS, bitmask);
 }
 
 static void reset_observe_tx(void)
 {
-    _write_reg(NRF24REG_OBSERVE_TX, 0);
+    __write_reg(NRF24REG_OBSERVE_TX, 0);
 }
 
 //[note] the NRF24CMD_R_RX_PL_WID command doesn't work?!
 static uint8_t get_top_rxfifo_width(void)
 {
-    uint8_t temp = NRF24CMD_R_RX_PL_WID;
+    uint8_t tmp = NRF24CMD_R_RX_PL_WID;
 
-    hal_nrf24l01_port.send_then_recv(&temp, 1, &temp, 1);
-    return temp;
+    hal_nrf24l01_port.send_then_recv(&tmp, 1, &tmp, 1);
+    return tmp;
 }
 
 // [note] enable activate-command befor enable_dpl
@@ -290,10 +288,10 @@ static void enable_dpl_and_ackpayload(uint8_t pipe)
     if (pipe > 5)
         return;
 
-    _set_reg_bits(NRF24REG_FEATURE, NRF24BITMASK_EN_ACK_PAY);
-    _set_reg_bits(NRF24REG_FEATURE, NRF24BITMASK_EN_DPL);
-    _set_reg_bits(NRF24REG_EN_AA, (1 << pipe));
-    _set_reg_bits(NRF24REG_DYNPD, (1 << pipe));
+    __set_reg_bits(NRF24REG_FEATURE, NRF24BITMASK_EN_ACK_PAY);
+    __set_reg_bits(NRF24REG_FEATURE, NRF24BITMASK_EN_DPL);
+    __set_reg_bits(NRF24REG_EN_AA, (1 << pipe));
+    __set_reg_bits(NRF24REG_DYNPD, (1 << pipe));
 }
 
 static void enabled_irq(uint8_t bitmask)
@@ -301,7 +299,7 @@ static void enabled_irq(uint8_t bitmask)
     if (!((bitmask == NRF24BITMASK_RX_DR) || (bitmask == NRF24BITMASK_TX_DS) || (bitmask == NRF24BITMASK_MAX_RT)))
         return;
 
-    _reset_reg_bits(NRF24REG_CONFIG, bitmask);
+    __reset_reg_bits(NRF24REG_CONFIG, bitmask);
 }
 
 static void disable_irq(uint8_t bitmask)
@@ -309,25 +307,25 @@ static void disable_irq(uint8_t bitmask)
     if (!((bitmask == NRF24BITMASK_RX_DR) || (bitmask == NRF24BITMASK_TX_DS) || (bitmask == NRF24BITMASK_MAX_RT)))
         return;
 
-    _set_reg_bits(NRF24REG_CONFIG, bitmask);
+    __set_reg_bits(NRF24REG_CONFIG, bitmask);
 }
 
 static void write_tx_payload(const uint8_t *pb, uint8_t len)
 {
-    uint8_t temp = NRF24CMD_W_TX_PAYLOAD;
+    uint8_t tmp = NRF24CMD_W_TX_PAYLOAD;
 
-    hal_nrf24l01_port.send_then_send(&temp, 1, pb, len);
+    hal_nrf24l01_port.send_then_send(&tmp, 1, pb, len);
 }
 
 static void write_ack_payload(uint8_t pipe, const uint8_t *pb, uint8_t len)
 {
-    uint8_t temp;
+    uint8_t tmp;
 
     if (pipe > 5)
         return;
 
-    temp = NRF24CMD_W_ACK_PAYLOAD | pipe;
-    hal_nrf24l01_port.send_then_send(&temp, 1, pb, len);
+    tmp = NRF24CMD_W_ACK_PAYLOAD | pipe;
+    hal_nrf24l01_port.send_then_send(&tmp, 1, pb, len);
 }
 
 static void read_rxpayload(uint8_t *pb, uint8_t len)
@@ -343,35 +341,35 @@ static void read_rxpayload(uint8_t *pb, uint8_t len)
 
 static void flush_tx_fifo(void)
 {
-    uint8_t temp = NRF24CMD_FLUSH_TX;
+    uint8_t tmp = NRF24CMD_FLUSH_TX;
 
-    hal_nrf24l01_port.write(&temp, 1);
+    hal_nrf24l01_port.write(&tmp, 1);
 }
 
 static void flush_rx_fifo(void)
 {
-    uint8_t temp = NRF24CMD_FLUSH_RX;
+    uint8_t tmp = NRF24CMD_FLUSH_RX;
 
-    hal_nrf24l01_port.write(&temp, 1);
+    hal_nrf24l01_port.write(&tmp, 1);
 }
 
 // note: will clear the error-counter
 uint16_t nrf24_get_errcnt(void)
 {
-    uint16_t temp = l_error_count;
+    uint16_t tmp = l_error_count;
     l_error_count = 0;
     
-    return temp;
+    return tmp;
 }
 
 void nrf24_power_up(void)
 {
-    _set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
+    __set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
 }
 
 void nrf24_power_down(void)
 {
-    _reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
+    __reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
 }
 
 /**
@@ -398,7 +396,7 @@ int nrf24_ptx_run(uint8_t *pb_rx, const uint8_t *pb_tx, uint8_t tlen)
 
     do
     {
-        sta = _read_reg(NRF24REG_STATUS);
+        sta = __read_reg(NRF24REG_STATUS);
         if (sta & NRF24BITMASK_MAX_RT)
         {
             // send failed; try again
@@ -445,7 +443,7 @@ int nrf24_prx_cycle(uint8_t *pb_rx, const uint8_t *pb_tx, uint8_t tlen)
 {
     uint8_t sta, rlen = 0;
 
-    sta = _read_reg(NRF24REG_FIFO_STATUS);
+    sta = __read_reg(NRF24REG_FIFO_STATUS);
     if (!(sta & NRF24BITMASK_RX_EMPTY))
     {
         rlen = get_top_rxfifo_width();
@@ -481,7 +479,7 @@ int nrf24_irq_ptx_run(uint8_t *pb_rx, const uint8_t *pb_tx, uint8_t tlen, void (
     write_tx_payload(pb_tx, tlen);
     hal_nrf24l01_port.set_ce();
     (*waitirq)();
-    sta = _read_reg(NRF24REG_STATUS);
+    sta = __read_reg(NRF24REG_STATUS);
     if (sta & NRF24BITMASK_TX_DS)
     {
         reset_status(NRF24BITMASK_TX_DS);
@@ -551,7 +549,7 @@ int nrf24_init(nrf24_cfg_t *pt)
     if ((pt->role != ROLE_PTX) && (pt->role != ROLE_PRX))
     {
         rt_kprintf("[nrf24-warning]: unknown ROLE\r\n");
-        _reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
+        __reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
         return -1;
     }
 
@@ -585,13 +583,13 @@ int nrf24_init(nrf24_cfg_t *pt)
 
     if (pt->role == ROLE_PTX)
     {
-        _set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
-        _reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PRIM_RX);
+        __set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
+        __reset_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PRIM_RX);
     }
     else if (pt->role == ROLE_PRX)
     {
-        _set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
-        _set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PRIM_RX);
+        __set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PWR_UP);
+        __set_reg_bits(NRF24REG_CONFIG, NRF24BITMASK_PRIM_RX);
         hal_nrf24l01_port.set_ce();
     }
     else
@@ -605,7 +603,7 @@ int nrf24_init(nrf24_cfg_t *pt)
 
 #if defined(NRF24_USING_INFO_REPORT)
 
-void _nrf24_report_config_reg(uint8_t data)
+void __nrf24_report_config_reg(uint8_t data)
 {
     if (data & NRF24BITMASK_PRIM_RX)
         rt_kprintf("PRX mode\r\n");
@@ -641,7 +639,7 @@ void _nrf24_report_config_reg(uint8_t data)
         rt_kprintf("power down now\r\n");
 }
 
-void _nrf24_report_enaa_reg(uint8_t data)
+void __nrf24_report_enaa_reg(uint8_t data)
 {
     if (!(data & 0x3F))
     {
@@ -663,7 +661,7 @@ void _nrf24_report_enaa_reg(uint8_t data)
         rt_kprintf("pipe5 ");
     rt_kprintf("AA opened\r\n");
 }
-void _nrf24_report_enrxaddr_reg(uint8_t data)
+void __nrf24_report_enrxaddr_reg(uint8_t data)
 {
     if (!(data & 0x3F))
     {
@@ -685,7 +683,7 @@ void _nrf24_report_enrxaddr_reg(uint8_t data)
         rt_kprintf("rx-pipe5 ");
     rt_kprintf(" opened\r\n");
 }
-void _nrf24_report_setupaw_reg(uint8_t data)
+void __nrf24_report_setupaw_reg(uint8_t data)
 {
     rt_kprintf("rx/tx address field width: ");
     switch (data & 0x3)
@@ -704,16 +702,16 @@ void _nrf24_report_setupaw_reg(uint8_t data)
         break;
     }
 }
-void _nrf24_report_setupretr_reg(uint8_t data)
+void __nrf24_report_setupretr_reg(uint8_t data)
 {
     rt_kprintf("auto retransmit delay: %dus\r\n", (((data & 0xF0) >> 4) + 1) * 250);
     rt_kprintf("auto retransmit count: up to %d\r\n", (data & 0x0F));
 }
-void _nrf24_report_rfch_reg(uint8_t data)
+void __nrf24_report_rfch_reg(uint8_t data)
 {
     rt_kprintf("rf channel: %d\r\n", data & 0x7F);
 }
-void _nrf24_report_rfsetup_reg(uint8_t data)
+void __nrf24_report_rfsetup_reg(uint8_t data)
 {
     rt_kprintf("air data rate: ");
     if (data & NRF24BITMASK_RF_DR)
@@ -738,7 +736,7 @@ void _nrf24_report_rfsetup_reg(uint8_t data)
         break;
     }
 }
-void _nrf24_report_status_reg(uint8_t data)
+void __nrf24_report_status_reg(uint8_t data)
 {
     rt_kprintf("status: ");
     if (data & NRF24BITMASK_RX_DR)
@@ -768,13 +766,13 @@ void _nrf24_report_status_reg(uint8_t data)
 
     rt_kprintf("\r\n");
 }
-void _nrf24_report_observetx_reg(uint8_t data)
+void __nrf24_report_observetx_reg(uint8_t data)
 {
     rt_kprintf("lost packets count: %d\r\n", (data & NRF24BITMASK_PLOS_CNT) >> 4);
     rt_kprintf("retransmitted packets count: %d\r\n", data & NRF24BITMASK_ARC_CNT);
 }
 
-void _nrf24_report_fifostatus_reg(uint8_t data)
+void __nrf24_report_fifostatus_reg(uint8_t data)
 {
     if (data & NRF24BITMASK_TX_REUSE)
         rt_kprintf("tx-reuse opened\r\n");
@@ -793,7 +791,7 @@ void _nrf24_report_fifostatus_reg(uint8_t data)
     else
         rt_kprintf("rx-fifo has some data\r\n");
 }
-void _nrf24_report_dynpd_reg(uint8_t data)
+void __nrf24_report_dynpd_reg(uint8_t data)
 {
     rt_kprintf("dynamic payload length enabled (pipe): ");
     if (!(data & 0x3F))
@@ -816,7 +814,7 @@ void _nrf24_report_dynpd_reg(uint8_t data)
         rt_kprintf("pipe5; ");
     rt_kprintf("\r\n");
 }
-void _nrf24_report_feature_reg(uint8_t data)
+void __nrf24_report_feature_reg(uint8_t data)
 {
     rt_kprintf("feature enabled conditions: ");
     if (data & NRF24BITMASK_EN_DPL)
@@ -830,19 +828,19 @@ void _nrf24_report_feature_reg(uint8_t data)
 }
 void nrf24_report(void)
 {
-    _nrf24_report_config_reg(_read_reg(NRF24REG_CONFIG));
-    _nrf24_report_enaa_reg(_read_reg(NRF24REG_EN_AA));
-    _nrf24_report_enrxaddr_reg(_read_reg(NRF24REG_EN_RXADDR));
-    _nrf24_report_setupaw_reg(_read_reg(NRF24REG_SETUP_AW));
-    _nrf24_report_setupretr_reg(_read_reg(NRF24REG_SETUP_RETR));
-    _nrf24_report_rfch_reg(_read_reg(NRF24REG_RF_CH));
-    _nrf24_report_rfsetup_reg(_read_reg(NRF24REG_RF_SETUP));
-    _nrf24_report_status_reg(_read_reg(NRF24REG_STATUS));
-    _nrf24_report_observetx_reg(_read_reg(NRF24REG_OBSERVE_TX));
+    __nrf24_report_config_reg(__read_reg(NRF24REG_CONFIG));
+    __nrf24_report_enaa_reg(__read_reg(NRF24REG_EN_AA));
+    __nrf24_report_enrxaddr_reg(__read_reg(NRF24REG_EN_RXADDR));
+    __nrf24_report_setupaw_reg(__read_reg(NRF24REG_SETUP_AW));
+    __nrf24_report_setupretr_reg(__read_reg(NRF24REG_SETUP_RETR));
+    __nrf24_report_rfch_reg(__read_reg(NRF24REG_RF_CH));
+    __nrf24_report_rfsetup_reg(__read_reg(NRF24REG_RF_SETUP));
+    __nrf24_report_status_reg(__read_reg(NRF24REG_STATUS));
+    __nrf24_report_observetx_reg(__read_reg(NRF24REG_OBSERVE_TX));
 
-    _nrf24_report_fifostatus_reg(_read_reg(NRF24REG_FIFO_STATUS));
-    _nrf24_report_dynpd_reg(_read_reg(NRF24REG_DYNPD));
-    _nrf24_report_feature_reg(_read_reg(NRF24REG_FEATURE));
+    __nrf24_report_fifostatus_reg(__read_reg(NRF24REG_FIFO_STATUS));
+    __nrf24_report_dynpd_reg(__read_reg(NRF24REG_DYNPD));
+    __nrf24_report_feature_reg(__read_reg(NRF24REG_FEATURE));
 }
 
 #endif // NRF24_USING_INFO_REPORT
@@ -879,7 +877,7 @@ static void nrf24(int argc, char **argv)
     if (!rt_strcmp(argv[1], "readreg"))
     {
         uint8_t reg = atoi(argv[2]);
-        rt_kprintf("reg:0x%x val: 0x%x\n", reg, _read_reg(reg));
+        rt_kprintf("reg:0x%x val: 0x%x\n", reg, __read_reg(reg));
     }
     if (!rt_strcmp(argv[1], "init"))
     {
@@ -904,7 +902,7 @@ static void nrf24(int argc, char **argv)
         {
             uint8_t reg = atoi(argv[2]);
             uint8_t data = atoi(argv[3]);
-            _write_reg(reg, data);
+            __write_reg(reg, data);
         }
     }
 }
